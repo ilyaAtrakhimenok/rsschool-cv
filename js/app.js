6c15174e23fa1421133b2670fee0c8343b92cdc3
Ilya Atrakhimenok <ilya.atrakhimenok@mail.ru>
2021-09-05 09:55:09 +0300

    feat: create new cv

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+let modalImg = document.getElementById('modal-img');
+let modal = document.getElementById('modal');
+let closeToggle = document.querySelector('.close');
+function zoomImg(elem){
+    modal.style.display = "block";
+    modalImg.src = elem.src;
+}
+function close(){
+    modal.style.display = "none";
+}
+
+closeToggle.addEventListener('click', close);
