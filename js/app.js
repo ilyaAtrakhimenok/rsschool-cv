4a850e179c64724d1afe226eac9559e4f27a4168
Ilya Atrakhimenok <ilya.atrakhimenok@mail.ru>
2021-09-05 11:52:42 +0300

    feat:add rsschool logo + github logo in footer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-
-let modalImg = document.getElementById('modal-img');
-let modal = document.getElementById('modal');
-let closeToggle = document.querySelector('.close');
-function zoomImg(elem){
-    modal.style.display = "block";
-    modalImg.src = elem.src;
+let modalImg = document.getElementById("modal-img");
+let modal = document.getElementById("modal");
+let closeToggle = document.querySelector(".close");
+function zoomImg(elem) {
+  modal.style.display = "block";
+  modalImg.src = elem.src;
 }
-function close(){
-    modal.style.display = "none";
+function close() {
+  modal.style.display = "none";
 }
 
-closeToggle.addEventListener('click', close);
+closeToggle.addEventListener("click", close);
